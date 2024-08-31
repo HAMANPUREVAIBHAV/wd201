103b066e3ecf9ee4f155df7c627dd6d8327ed943
HAMANPUREVAIBHAV <21951a05n5@iare.ac.in>
2024-08-31 12:03:05 +0530

    pre commit hook

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,28 @@
+const todoList = require("../todo");
+const  {all, markAsComplete, add} = todoList();
+
+describe("Todolist test suite", () => {
+    beforeAll(()=>{
+        add({
+            title: "Test todo",
+            completed: false,
+            dueDate:new Date().toISOString().slice(0, 10)
+        });
+    })
+    test("Should add a new todo", () => {
+        const todoListCount = all.length
+        add({
+            title: "Test todo",
+            completed: false,
+            dueDate:new Date().toISOString().slice(0, 10)
+        });
+        expect(all.length).toBe(todoListCount+1);  // Validate that the todo was added
+    });
+    test("Should mark todo as compelete", ()=>{
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+
+    })
+});
+
